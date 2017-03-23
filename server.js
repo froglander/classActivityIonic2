82a6a5b1365d9befd35fd972d552bfedc08cd8eb
Kristi Heredia <froglander@dukworld.net>
2017-03-23 19:57:41 -0400

    heroku fun 2

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 var express = require('express'),
     app = express();
 app.use(express.static('www'));
-app.set('port', process.env.PORT || 5000);
+console.log("port:", process.env.PORT);
+app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
